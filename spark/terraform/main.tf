090b12c94ae61bc1fe2ba1e187f863f19911d2dc
Jose Chu <jose.chu.dev@gmail.com>
2022-11-02 15:34:26 -0500

    Spark cluster terraform

---
+++ spark/terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.29.1"
+      }
+    }
+}
+
+
+module "cluster" {
+  source = "./modules/cluster/"
+  serviceprincple_id = var.serviceprinciple_id
+  serviceprinciple_key = var.serviceprinciple_key
+  ssh_key = var.ssh_key
+  location = var.location
+  kubernetes_version = var.kubernetes_version
+}
